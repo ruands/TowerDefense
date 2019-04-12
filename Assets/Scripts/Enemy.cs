073ce4f8eef5682dc71d746c23809e0d783629f9
Ruan de Sousa <ruands@users.noreply.github.com>
2019-04-12 21:01:09 +0200

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float speed = 10f;
+    [SerializeField] float targetAcceptance = 0.5f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start() {
+        target = Waypoints.points[waypointIndex];
+    }
+
+    private void Update() {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= targetAcceptance) {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint() {
+        if (waypointIndex >= Waypoints.points.Length - 1) {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    [SerializeField] Transform enemyPrefab;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] Text waveCountdownText;
+    [SerializeField] Text waveCountText;
+    [SerializeField] float timeBetweenWaves = 5f;
+    [SerializeField] float countdown = 2f;
+    [SerializeField] int numberOfWaves = 5;
+
+    private int waveIndex = 0;
+
+    private void Update() {
+        if (waveIndex < numberOfWaves) {
+            if (countdown <= 0f) {
+                StartCoroutine(SpawnWave());
+                countdown = timeBetweenWaves;
+            }
+            countdown -= Time.deltaTime;
+            waveCountdownText.text = Mathf.Round(countdown).ToString();
+        } else {
+            waveCountdownText.text = "0";
+        }
+        waveCountText.text = string.Format("{0} / {1}", waveIndex, numberOfWaves);
+    }
+
+    IEnumerator SpawnWave() {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
