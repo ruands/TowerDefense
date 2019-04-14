1cae26e895bf683ad6c6028efe995d5e0de69356
Ruan de Sousa <ruands@users.noreply.github.com>
2019-04-14 19:37:37 +0200

    Building turrets

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    void Awake() {
+        if (instance != null) {
+            Debug.LogError("More than one BuildManager in scene.");
+        }
+        instance = this;
+    }
+
+    void Start() {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild() { return turretToBuild; }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    [SerializeField] Color hoverColor;
+    [SerializeField] Vector3 offset;
+
+    private GameObject turret;
+
+    private Color originalColor;
+    private Renderer rend;
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+        originalColor = rend.material.color;
+    }
+
+    void OnMouseDown() {
+        if(turret != null) {
+            Debug.Log("Space Occupied");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject) Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+    }
+
+	void OnMouseEnter() {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit() {
+        rend.material.color = originalColor;
+    }
+}
