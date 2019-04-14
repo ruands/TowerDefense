fef09e9947b5ef7029f6986c964fee97dbba236b
Ruan de Sousa <ruands@users.noreply.github.com>
2019-04-14 09:12:46 +0200

    Basic camera movement
    No limits on the movement.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 10f;
+	[SerializeField] float mouseScrollSpeed = 50f;
+	
+	void Update () {
+        float forwardBackward = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		float verticalMovement = Input.GetAxis("Mouse ScrollWheel") * mouseScrollSpeed * Time.deltaTime;
+
+        Vector3 newPosition = new Vector3(horizontalMovement, -verticalMovement, forwardBackward);
+        transform.Translate(newPosition, Space.World);
+	}
+}
