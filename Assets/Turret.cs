3e142d8e4c93e6dfe444ef62bac118c1f31e956d
Ruan de Sousa <ruands@users.noreply.github.com>
2019-04-14 14:34:33 +0200

    Turret targetting enemies

---
+++ Assets/Turret.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    [SerializeField] float range = 15f;
+    [SerializeField] string enemyTag = "Enemy";
+    [SerializeField] Transform partToRotate;
+    [SerializeField] float turnSpeed = 10f;
+
+    private Transform target;
+
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+    void UpdateTarget() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies) {
+            float distanceEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceEnemy < shortestDistance) {
+                shortestDistance = distanceEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range) {
+            target = nearestEnemy.transform;
+        }
+        else {
+            target = null;
+        }
+    }
+	
+	void Update () {
+		if (target == null) return;
+
+        // Target lock on
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+	}
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
