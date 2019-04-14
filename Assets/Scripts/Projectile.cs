289cbc37065a8408ee57fc7653e4026264e36fe8
Ruan de Sousa <ruands@users.noreply.github.com>
2019-04-14 15:13:31 +0200

    Turrets can shoot

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField] float speed = 70f;
+    [SerializeField] GameObject impactEffect;
+    private Transform target;
+
+    public void Seek(Transform _target) {
+        target = _target;
+    }
+
+	void Update () {
+		if (target == null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame) {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+	}
+
+    private void HitTarget() {
+        GameObject effectInstance = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectInstance, 2f);
+        Destroy(gameObject);
+
+        Destroy(target.gameObject);
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Turret : MonoBehaviour {
 
+    [Header("Shooting")]
     [SerializeField] float range = 15f;
-    [SerializeField] string enemyTag = "Enemy";
+    [SerializeField] float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Turret")]
     [SerializeField] Transform partToRotate;
     [SerializeField] float turnSpeed = 10f;
 
+    [Header("Target")]
+    [SerializeField] string enemyTag = "Enemy";
     private Transform target;
+
+    [Header("Projectile")]
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] Transform firePoint;
 
 	void Start () {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -44,7 +55,22 @@ public class Turret : MonoBehaviour {
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f) {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
 	}
+
+    private void Shoot() {
+        GameObject projectileGO = (GameObject) Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Projectile projectile = projectileGO.GetComponent<Projectile>();
+        if (projectile != null) {
+            projectile.Seek(target);
+        }
+    }
 
     void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
